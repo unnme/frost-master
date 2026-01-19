f70b142015726ee629d4c6a309732c123dca18a8
unnme <unnme@icloud.com>
2026-01-19 13:54:26 +0300

    chore: implement Content Security Policy, update robots.txt for Yandex Images, add privacy page to sitemap, and enhance GMap component with lazy loading

--- frontend/src/components/home/contactsSection/GMap.jsx
+++ frontend/src/components/home/contactsSection/GMap.jsx
@@ -1,4 +1,4 @@
-import { ContactCard } from "./ContactCard";
+import { ContactCard } from "./ContactCard"
 
 export const GMap = () => {
   return (
@@ -9,6 +9,7 @@ export const GMap = () => {
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2251.5602091870833!2d38.91569687140184!3d45.015807029397365!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40f04ef38204d6e9%3A0x10c6a2de7f111f3e!2z0JHQttC10LPQvtC60LDQudGB0LrQsNGPLCAzMSDQutC-0YDQv9GD0YEgMywg0J3QvtCy0LDRjyDQkNC00YvQs9C10Y8sINCg0LXRgdC_0YPQsdC70LjQutCwINCQ0LTRi9Cz0LXRjywgMzg1MTQw!5e0!3m2!1sru!2sru!4v1765256936936!5m2!1sru!2sru"
           className="h-[450px] w-full"
           allowFullScreen
+          loading="lazy"
           referrerPolicy="no-referrer-when-downgrade"
         />
 
